b2acfa594fa92f3c8fe14dce7fdc333cb7628264
Radu Iacob <johnthebrave@users.noreply.github.com>
2018-12-03 12:23:53 +0200

    Update algo.h

--- Problem 5.2 - Shortest Path - All To All/algo.h
+++ Problem 5.2 - Shortest Path - All To All/algo.h
@@ -13,7 +13,8 @@ typedef std::pair<int, int> edge;
  * For a given input graph T, computes the distance from every node to every other node in the graph.
  *
  * @graph: The graph is represented using adjacency lists (graph[i] - gives you the neighbours of node i) *
- * @returns: The adjacency matrix with the minimum distances between every node.
+ * @returns: The adjacency matrix with the minimum distances between every node. If there is no way
+ *           reach node j from i, store -1 in the matrix. (print it as "inf" in the tests)
  */
 std::vector<std::vector<int>> shortest_path_all(const std::vector<std::vector<edge>>& graph);
 

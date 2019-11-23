caced687ea2fe897019a421eb9d54ef86313761a
Radu Iacob <johnthebrave@users.noreply.github.com>
2019-11-23 18:12:23 +0200

    Update algo.h
    Fixed bad documentation.

--- Problem 9 - Data Compression/algo.h
+++ Problem 9 - Data Compression/algo.h
@@ -14,8 +14,8 @@ std::vector<unsigned char> compress(const std::vector<unsigned char>& content);
 /**
  * Decompresses the data it receives.
  *
- * @content: The original content of a file in bytes
- * @returns: The content of the compressed file
+ * @content: The compressed file in bytes
+ * @returns: The content of the original file
  */
 std::vector<unsigned char> decompress(const std::vector<unsigned char>& content);
 
